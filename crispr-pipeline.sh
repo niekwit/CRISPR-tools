7d3586ef894efedc4e74bdbd762ff7b2b4f643e2
niekwit <niekwit@gmail.com>
2020-12-14 09:05:58 +0000

    added clipper function for truncated libraries

--- crispr-pipeline.sh
+++ crispr-pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-### Author: Niek Wit (University of Cambridge), 2020 ###
+### Author: Niek Wit (University of Cambridge) 2020 ###
 
 file_path=""
 library=""
@@ -36,21 +36,27 @@ if [ $library = "bassik" ];
 	then
 		index_path="/home/niek/Documents/references/bowtie-index/CRISPR/Bassik/bowtie-lib/bassik-bowtie"
 		guides="/home/niek/Documents/references/bowtie-index/CRISPR/Bassik/bowtie-lib/bassik-guides-sorted.csv"
+		read_mod="clip"
+		clip_seq="GTTTAAGAGCTAAGCTGGAAACAGCATAGCAA"
 		echo "Bassik library selected"
+		
 elif [ $library = "moffat_tko1" ];
 	then
 		index_path="/home/niek/Documents/references/bowtie-index/CRISPR/Moffat_TKO1/moffat_TKO1-index"
-		guides="/home/niek/Documents/references/bowtie-index/CRISPR/Moffat_TKO1/moffat-guideslist-sorted.csv"	
+		guides="/home/niek/Documents/references/bowtie-index/CRISPR/Moffat_TKO1/moffat-guideslist-sorted.csv"
+		read_mod="trim"	
 		echo "Moffat TKO1 library selected"
 elif [ $library = "sabatini" ];
 	then
 		index_path="/home/niek/Documents/references/bowtie-index/CRISPR/Sabatini/sabatini-index"
-		guides="/home/niek/Documents/references/bowtie-index/CRISPR/Sabatini/sabatini-guides-names.csv"	
+		guides="/home/niek/Documents/references/bowtie-index/CRISPR/Sabatini/sabatini-guides-names.csv"
+		read_mod="trim"	
 		echo "Sabatini library selected"
 elif [ $library = "dub-only" ];
 	then
 		index_path="/home/niek/Documents/references/bowtie-index/CRISPR/DUB-only-lib/DUB_only-index"
 		guides="/home/niek/Documents/references/bowtie-index/CRISPR/DUB-only-lib/DUBlibrarysgRNAname.csv"
+		read_mod="trim"
 		echo "DUB only library selected"
 fi
 
@@ -85,15 +91,30 @@ multiqc -o fastqc/ fastqc/ . 2> crispr.log
 
 #Trims, aligns and counts reads
 echo "Aligning reads to reference"
-for file in raw-data/*.fastq
-do 
-	echo $file >> crispr.log
-	file_name=${file##*/} #substring removal of file location
-	file2=${file_name%.fastq} #substring removal of .fastq
-	extension=".guidecounts.txt"
-	output_file=$file2$extension
-	cat $file | fastx_trimmer -l 20 -Q33 2>> crispr.log | bowtie --sam-nohead -5 0 -p40 -t -v0 $index_path - 2>> crispr.log | sed '/XS:/d' | cut -f3 | sort | uniq -c > count/$output_file
-done
+
+if [ $read_mod == "trim" ];
+	then
+		for file in raw-data/*.fastq
+		do 
+			echo $file >> crispr.log
+			file_name=${file##*/} #substring removal of file location
+			file2=${file_name%.fastq} #substring removal of .fastq
+			extension=".guidecounts.txt"
+			output_file=$file2$extension
+			cat $file | fastx_trimmer -l 20 -Q33 2>> crispr.log | bowtie --sam-nohead -5 0 -p40 -t -v0 $index_path - 2>> crispr.log | sed '/XS:/d' | cut -f3 | sort | uniq -c > count/$output_file
+		done
+elif [ $read_mod == "clip" ];
+	then
+		for file in raw-data/*.fastq
+		do 
+			echo $file >> crispr.log
+			file_name=${file##*/} #substring removal of file location
+			file2=${file_name%.fastq} #substring removal of .fastq
+			extension=".guidecounts.txt"
+			output_file=$file2$extension
+			cat $file | fastx_clipper -Q33 -l 12 -a $clip_seq -v -n 2>> crispr.log | bowtie --sam-nohead -5 0 -p40 -t -v0 $index_path - 2>> crispr.log | sed '/XS:/d' | cut -f3 | sort | uniq -c > count/$output_file
+		done
+fi
 
 cp /home/niek/Documents/scripts/CRISPR-tools/mageck-join.py count/
 
