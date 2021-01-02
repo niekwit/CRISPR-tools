ca2a9b17bdda71f8c63b07c0a897046be8ff7014
niekwit <niekwit@gmail.com>
2021-01-02 21:40:22 +0000

    updated error message

--- guide-names.py
+++ guide-names.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-docstring="""
-@author: Niek Wit (University of Cambridge)
-This function returns a .csv file of guides names extracted from the FASTA CRISPR library file
-Run as follows from the command line: $ python3 /path/to/guide-names.py library.fasta"""
+##@author: Niek Wit (University of Cambridge)
+##This function returns a .csv file (in the folder from where it is run) of guides names extracted from the FASTA CRISPR library file
+docstring="""\nUsage: $ python3 /path/to/guide-names.py /path/to/library.fasta"""
 
 import pandas as pd
 import sys
 
 if len(sys.argv) != 2:
-    sys.exit('\nJust one argument required (see instructions)%s' %(docstring))
+    sys.exit('Error: one argument required%s' %(docstring))
 
 library = pd.read_csv(sys.argv[1], names = ['guide'])
 #creates new dataframe with only guide names:
