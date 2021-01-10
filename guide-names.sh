c6a23d8cb5ea7fe1d54a59f24136e48a7d7c6eff
niekwit <niekwit@gmail.com>
2021-01-10 20:06:05 +0000

    bash version of Python script

---
+++ guide-names.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -
+
+#Converts a CRISPR library fasta file to just guide names (CSV) for use in CRISPR pipeline
+
+if (($# != 1));
+	then 
+		>&2 echo "Error. Usage: $ ./guide-names.sh <library.fasta>"
+		exit
+fi
+
+file_out=$1
+file_out="${file_out%.fasta}.csv"
+cat *.fasta | grep ">" | tr -d ">" > $file_out
