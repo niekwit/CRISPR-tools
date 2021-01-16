8090f9e718b004b440e0927068f7f075c95ac37e
niekwit <niekwit@gmail.com>
2021-01-16 20:52:23 +0000

    enables autocompletion of library options

---
+++ auto-complete.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#This script enables autocompletion for the CRISPR library options (-l) in crispr-pipeline.sh
+
+function libs()
+{
+  # $1 is the name of the command 
+  # $2 is the word being completed
+  # $3 is the word preceding the word being completed
+
+case $3 in
+    -l) COMPREPLY=($(compgen -W "bassik moffat_tko1 sabatini dub-only" "${COMP_WORDS[$COMP_CWORD]}"));;
+  esac
+}
+
+complete -F libs crispr-pipeline.sh
