78ba17a6a57d35bdb9cfdb6b12d585acc730fe9b
niekwit <niekwit@gmail.com>
2021-02-02 20:58:10 +0000

    added progress indication during alignment

--- crispr-pipeline.sh
+++ crispr-pipeline.sh
@@ -125,26 +125,36 @@ multiqc -o fastqc/ fastqc/ . 2> crispr.log
 echo "Aligning reads to reference ($align_mm mismatch(es) allowed)"
 if [ $read_mod == "trim" ];
 	then
+		count=0
+		totalfiles=$(ls raw-data/*$input_extension | wc -l)
 		for file in raw-data/*$input_extension
 		do 
+			count=$(($count + 1))
 			echo $file >> crispr.log
 			file_name=${file##*/} #substring removal of file location
 			file2=${file_name%$input_extension} #substring removal of file extension
 			extension=".guidecounts.txt"
 			output_file=$file2$extension
+			echo -ne "\r$count/$totalfiles"
 			cutadapt -j $max_threads --quality-base 33 -l 20 -o - $file 2>> crispr.log | bowtie2 --no-hd -p $max_threads -t -N $align_mm -x $index_path - 2>> crispr.log | sed '/XS:/d' | cut -f3 | sort | uniq -c > count/$output_file
 		done
+		echo -e \
 elif [ $read_mod == "clip" ];
 	then
+		count=0
+		totalfiles=$(ls raw-data/*$input_extension | wc -l)		
 		for file in raw-data/$input_extension
 		do 
+			count=$(($count + 1))			
 			echo $file >> crispr.log
 			file_name=${file##*/} #substring removal of file location
 			file2=${file_name%$input_extension} #substring removal of file extension
 			extension=".guidecounts.txt"
 			output_file=$file2$extension
+			echo -ne "\r$count/$totalfiles"
 			cutadapt -j $max_threads --quality-base 33 -a $clip_seq -o - $file 2>> crispr.log | bowtie2 --trim5 1 --no-hd -p $max_threads -t -N $align_mm -x $index_path - 2>> crispr.log | sed '/XS:/d' | cut -f3 | sort | uniq -c > count/$output_file
 		done
+		echo -e \
 fi
 
 #removes first line from guide count text files (bowtie2 artefact)
