c45e3430960a2bb72e296be79710614ff7bfeace
niekwit <41302859+niekwit@users.noreply.github.com>
2021-02-09 16:35:42 +0000

    bug fixes

--- crispr-pipeline.sh
+++ crispr-pipeline.sh
@@ -7,11 +7,12 @@ rename_config="NULL"
 remove_fq=""
 align_mm=0
 
-SCRIPT_DIR="/home/niek/Documents/scripts/CRISPR-tools/"
+SCRIPT_DIR=$(find $HOME -type d -name "CRISPR-tools")
+source library.conf #loads library location variables
 
 usage() {                                    
   echo "Usage: $0 [ -l <CRISPR library> ] [ -r <rename.config> OPTIONAL] [-m <INT> number of mismatches allowed for alignment (standard is zero) OPTIONAL]"
-  echo -e "CRISPR library options:\nbassik (Morgens et al 2017 Nat. Comm.)\nmoffat_tko1 (Hart et al 2015 Cell)\nmoffat_tko3 (Hart et al 2017 G3/Mair et al 2019 Cell Rep)\nsabatini (Park et al 2016 Nat. Gen.)\ndub-only (Nathan lab, unpublished)"
+  echo -e "CRISPR library options:\nbassik (Morgens et al 2017 Nat. Comm.)\nmoffat_tko1 (Hart et al 2015 Cell)\nmoffat_tko3 (Hart et al 2017 G3/Mair et al 2019 Cell Rep)\nsabatini (Park et al 2016 Nat. Gen.)\ndub-only (Nathan lab, unpublished)\nslc-mito-2ogdd (Nathan lab, unpublished)"
   exit 2
 }
 
@@ -41,46 +42,54 @@ else
     exit 1
 fi
 
-if [ $library = "bassik" ];
+if [[ $library = "bassik" ]];
 	then
-		index_path="/home/niek/Documents/references/bowtie2-index/bassik/bassik"
-		guides="/home/niek/Documents/references/bowtie-index/CRISPR/Bassik/bowtie-lib/bassik-guides-sorted.csv"
-		read_mod="clip"
-		clip_seq="GTTTAAGAGCTAAGCTGGAAACAGCATAGCAA"
-		species="human"
+		index_path="$index_path1"
+		guides="$guides1"
+		read_mod="$read_mod1"
+		clip_seq="$clip_seq1"
+		species="$species1"
 		echo "Bassik library selected"
-elif [ $library = "moffat_tko1" ];
+elif [[ $library = "moffat_tko1" ]];
 	then
-		index_path="/home/niek/Documents/references/bowtie2-index/moffat_tko1/moffat_tko1"
-		guides="/home/niek/Documents/references/bowtie-index/CRISPR/Moffat_TKO1/moffat-guideslist-sorted.csv"
-		read_mod="trim"
-		sg_length=20
-		species="human"
+		index_path="$index_path2"
+		guides="$guides2"
+		read_mod="$read_mod2"
+		sg_length="$sg_length2"
+		species="$species2"
 		echo "Moffat TKO1 library selected"
-elif [ $library = "moffat_tko3" ];
+elif [[ $library = "moffat_tko3" ]];
 	then
-		index_path="/home/niek/Documents/references/bowtie2-index/moffat_tko3/moffat_tko3"
-		guides="/home/niek/Documents/references/bowtie2-index/moffat_tko3/Moffat_TKO3-guide-names.csv"
-		read_mod="trim"
-		sg_length=20
-		species="human"	
+		index_path="$index_path3"
+		guides="$guides3"
+		read_mod="$read_mod3"
+		sg_length="$sg_length3"
+		species="$species3"	
 		echo "Moffat TKO3 library selected"
-elif [ $library = "sabatini" ];
+elif [[ $library = "sabatini" ]];
 	then
-		index_path="/home/niek/Documents/references/bowtie2-index/sabatini/sabatini"
-		guides="/home/niek/Documents/references/bowtie-index/CRISPR/Sabatini/sabatini-guides-names.csv"
-		read_mod="trim"
-		sg_length=20
-		species="human"	
+		index_path="$index_path4"
+		guides="$guides4"
+		read_mod="$read_mod4"
+		sg_length="$sg_length4"
+		species="$species4"	
 		echo "Sabatini library selected"
-elif [ $library = "dub-only" ];
+elif [[ $library = "dub-only" ]];
 	then
-		index_path="/home/niek/Documents/references/bowtie2-index/dub-only/bowtie2-dub-only-index"
-		guides="/home/niek/Documents/references/bowtie-index/CRISPR/DUB-only-lib/DUBlibrarysgRNAname.csv"
-		read_mod="trim"
-		sg_length=20
-		species="human"
+		index_path="$index_path5"
+		guides="$guides5"
+		read_mod="$read_mod5"
+		sg_length="$sg_length5"
+		species="$species5"
 		echo "DUB only library selected"
+elif [[ $library = "slc-mito-2ogdd" ]];
+	then
+		index_path="$index_path6"
+		guides="$guides6"
+		read_mod="$read_mod6"
+		clip_seq="$clip_seq6"
+		species="$species6"
+		echo "SLC, 2-OG DD, mitoP sub library selected (mouse)"
 fi
 
 start_time=$(date +%s)
@@ -93,17 +102,14 @@ if [ $rename_config != "NULL" ];
 		input=$rename_config
 		while IFS= read -r line
 		do
-		  original_file=$(echo "$line" | cut -d ";" -f 1) #splits line of config file into original file name
-		  new_file=$(echo "$line" | cut -d ";" -f 2) #splits line of config file into new file name
-		  mv "raw-data/${original_file}" "raw-data/${new_file}"
+			original_file=$(echo "$line" | cut -d ";" -f 1) #splits line of config file into original file name
+			new_file=$(echo "$line" | cut -d ";" -f 2) #splits line of config file into new file name
+			mv "raw-data/${original_file}" "raw-data/${new_file}"
 		done < "$input"
-	else
-		:
 fi
 
 #checks if file extension is .fq or .fastq
 input_format=$(ls raw-data/*.gz | head -1)
-
 if [[ $input_format == *"fastq"* ]]
 	then
   		input_extension=".fastq.gz"
@@ -143,7 +149,7 @@ elif [ $read_mod == "clip" ];
 	then
 		count=0
 		totalfiles=$(ls raw-data/*$input_extension | wc -l)		
-		for file in raw-data/$input_extension
+		for file in raw-data/*$input_extension
 		do 
 			count=$(($count + 1))			
 			echo $file >> crispr.log
@@ -163,7 +169,7 @@ do
 	sed '1d' $file > "$file.temp" && mv "$file.temp" $file
 done
 
-cp "${SCRIPT_DIR}mageck-join.py" count/
+cp "${SCRIPT_DIR}/mageck-join.py" count/
 
 #Creates MAGeCK input file
 cd count/
@@ -171,7 +177,7 @@ python3 mageck-join.py $guides
 
 #Normalises MAGeCK input file to total read count
 working_dir=$(pwd)
-Rscript "${SCRIPT_DIR}normalise.R" $working_dir
+Rscript "${SCRIPT_DIR}/normalise.R" $working_dir
 
 cp counts-aggregated.tsv ../mageck/
 
@@ -193,35 +199,44 @@ fi
 sep="\t"
 mageck_test=$(awk -F"${sep}" '{print NF-1}' <<< "${test_line}")
 
-if [[ "$test_line" == *"pre"* ]] && [[ "$test_line" == *"post"* ]] && [[ $mageck_test == 3 ]]; 
+if [[ "$test_line" == *"pre"* ]] && [[ "$test_line" == *"post"* ]] && [[ $mageck_test == 3 ]] ; 
 	then
   		rm -r ../mageck
   		echo "No MAGeCK analysis performed"
-	else
-		cd ../mageck
-		sed '1d' ../mageck.config > mageck.config #removes header from config file
-		echo "Performing MAGeCK"
-		input="mageck.config"
-		while IFS= read -r line
-		do
- 		 	test_sample=$(echo "$line" | cut -d ";" -f 1) #splits line of config file into test sample name
-  			control_sample=$(echo "$line" | cut -d ";" -f 2) #splits line of config file into control sample name
-  			mageck_output="${test_sample}_vs_${control_sample}"
-  			mkdir $mageck_output
-  			mageck test -k counts-aggregated.tsv -t $test_sample -c $control_sample --sort-criteria neg -n $mageck_output/$mageck_output 2>> ../crispr.log
-		done < "$input"	
+elif [[ ! -e ../mageck.config ]];
+	then
+		rm -r ../mageck
+		echo "No MAGeCK analysis performed (mageck.config missing)"
+else
+	cd ../mageck
+	sed '1d' ../mageck.config > mageck.config #removes header from config file
+	echo "Performing MAGeCK"
+	input="mageck.config"
+	while IFS= read -r line
+	do
+	 	test_sample=$(echo "$line" | cut -d ";" -f 1) #splits line of config file into test sample name
+		control_sample=$(echo "$line" | cut -d ";" -f 2) #splits line of config file into control sample name
+		mageck_output="${test_sample}_vs_${control_sample}"
+		mkdir $mageck_output
+		mageck test -k counts-aggregated.tsv -t $test_sample -c $control_sample --sort-criteria neg -n $mageck_output/$mageck_output 2>> ../crispr.log
+	done < "$input"	
 fi
 
 #plots results and marks top 10 hits, and performs GO analysis of signficant genes (standard FDR cut off is set at 0.25)
 fdr=0.25
 file_list=$(find . -name "*gene_summary.txt") #lists all paths to MAGeCK output files
 
-for file in $file_list
+if [[ -z "$file_list" ]];
+	then
+		:
+else
+	for file in $file_list
 		do 
 			save_path=$(echo $file | sed 's|\(.*\)/.*|\1|') #removes file name from $file 
 			save_path="${save_path}/"
 			Rscript "${SCRIPT_DIR}plot-hits.R" $fdr $file $species $save_path 
 		done
+fi
 
 end_time=$(date +%s)
 runtime=$((end_time-start_time))
