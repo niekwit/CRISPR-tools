577fdb373885841552b95796c5c7af06758de0de
niekwit <41302859+niekwit@users.noreply.github.com>
2021-02-09 16:35:19 +0000

    added new sub library

--- auto-complete.sh
+++ auto-complete.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #This script enables autocompletion for the CRISPR library options (-l) in crispr-pipeline.sh
+#Add the following line to your .bashrc file: source /path/to/CRISPR-tools/auto-complete.sh
 
 function libs()
 {
@@ -9,8 +10,8 @@ function libs()
   # $3 is the word preceding the word being completed
 
 case $3 in
-    -l) COMPREPLY=($(compgen -W "bassik moffat_tko1 moffat_tko3 sabatini dub-only" "${COMP_WORDS[$COMP_CWORD]}"));;
+    -l) COMPREPLY=($(compgen -W "bassik moffat_tko1 moffat_tko3 sabatini dub-only slc-mito-2ogdd" "${COMP_WORDS[$COMP_CWORD]}"));;
   esac
 }
 
-complete -F libs crispr-pipeline #refers to symbolic link in /usr/bin
+complete -F libs crispr-pipeline.sh
