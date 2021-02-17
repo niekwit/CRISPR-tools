6127ea335ae685f574cf5027bb75439168cc80c0
niekwit <41302859+niekwit@users.noreply.github.com>
2021-02-17 10:49:04 +0000

    detects libraries from yaml

--- auto-complete.sh
+++ auto-complete.sh
@@ -3,14 +3,16 @@
 #This script enables autocompletion for the CRISPR library options (-l) in crispr-pipeline.sh
 #Add the following line to your .bashrc file: source /path/to/CRISPR-tools/auto-complete.sh
 
+#finds CRISPR libary names
+SCRIPT_DIR=$(find $HOME -type d -name "CRISPR-tools")
+lib_list=$(cat "$SCRIPT_DIR/config.yml" | shyaml keys | tr "\n" " ")
+lib_list=$(echo ${lib_list/mageck-fdr /})
+
+#enables autocompletion of `-l` flag
 function libs()
 {
-  # $1 is the name of the command 
-  # $2 is the word being completed
-  # $3 is the word preceding the word being completed
-
 case $3 in
-    -l) COMPREPLY=($(compgen -W "bassik moffat_tko1 moffat_tko3 sabatini dub-only slc-mito-2ogdd" "${COMP_WORDS[$COMP_CWORD]}"));;
+    -l) COMPREPLY=($(compgen -W "$lib_list" "${COMP_WORDS[$COMP_CWORD]}"));;
   esac
 }
 
