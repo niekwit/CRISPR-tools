7a06384b7da7dfe5c09f0fb9141d033993cb5d55
niekwit <41302859+niekwit@users.noreply.github.com>
2021-04-21 08:40:02 +0100

    fixed issue with missing values

--- normalise.R
+++ normalise.R
@@ -11,7 +11,7 @@ df <- read.csv(file="counts-aggregated.tsv", sep= '\t')
 
 df.output <- df
 for(x in 3:ncol(df)) {
-  sum.column <- sum(df[x])
+  sum.column <- sum(df[x],na.rm=TRUE)
   df.output[x] <-ceiling(df[x]/sum.column*1E8)
 }
 
