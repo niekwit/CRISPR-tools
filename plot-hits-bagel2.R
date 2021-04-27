c12c3800dc22c579f1ee23331413247b85a33cf2
niekwit <41302859+niekwit@users.noreply.github.com>
2021-04-27 15:56:21 +0100

    added precision-recall plot (needs work)

--- plot-hits-bagel2.R
+++ plot-hits-bagel2.R
@@ -15,6 +15,7 @@ df.top.ten <- df[1:10, ]
 df.hits <- df[df$BF>5, ]
 df <- arrange(df, df$Gene)
 
+#plot hits
 p <- ggplot(df, aes(x=`Gene`,y=`BF`)) +
   theme_bw() +
   theme(axis.text = element_text(size=16),
@@ -30,7 +31,7 @@ p <- ggplot(df, aes(x=`Gene`,y=`BF`)) +
   ylab("log2 Bayes Factor") +
   geom_point(df, mapping=aes(size=`Precision`, fill=`Recall`)
              ,shape=21) +
-  ggtitle(title)+
+  ggtitle(title) +
   scale_fill_viridis(discrete=FALSE,
                      guide = guide_colorbar(frame.colour = "black", 
                                                           ticks.colour = "black"), 
@@ -41,8 +42,6 @@ p <- ggplot(df, aes(x=`Gene`,y=`BF`)) +
   geom_label_repel(data = df.hits, 
                    aes(x = `Gene`, y = `BF`, label = `Gene`))
 
-
-
 file.plot <- paste0("/bagel2-hits",title, ".pdf")
 
 ggsave(plot=p,
@@ -50,3 +49,28 @@ ggsave(plot=p,
      width = 7,
      height = 5,
      useDingbats = FALSE)
+
+df <- arrange(df, df$BF)
+
+#plot Precision-Recall plot
+pp <- ggplot(df, aes(x=`Recall`,y=`Precision`)) +
+  theme_bw() +
+  theme(axis.text = element_text(size=16),
+        axis.title = element_text(size=16),
+        plot.title = element_text(hjust = 0.5),
+        axis.line = element_line(colour = "black"),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.major = element_blank()) +
+  xlab("Recall") +
+  ylab("Precision (1-FDR)") +
+  geom_line(df, mapping=aes(colour="navy")) +
+  ggtitle(paste0("Precision-Recall plot ",title))
+
+file.plot <- paste0("/precision-recall-",title, ".pdf")
+
+ggsave(plot=pp,
+       file=paste0(save.path,file.plot),
+       width = 7,
+       height = 5,
+       useDingbats = FALSE)
