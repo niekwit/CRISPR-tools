60ee26c41a4c6136e76a4ebe3221909acd575a32
niekwit <41302859+niekwit@users.noreply.github.com>
2021-04-28 11:04:35 +0100

    removes empty lines in config file

--- mageck.sh
+++ mageck.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-working_dir=$1
+fdr=$1
+go_analysis=$2
+go_test=$3
+go_term=$4
+SCRIPT_DIR=$5
+email=$6
+species=$7
+working_dir=$8
 
 mkdir -p "$working_dir/mageck"
-cd "$working_dir"
 
 ###Performs MAGeCK
 #-c reference sample, -t test sample: neg rank(genes that drop out in test sample)/pos rank(genes that are overrepresented in test sample)
@@ -17,13 +23,14 @@ if [[ "$test_line" == *"pre"* ]] && [[ "$test_line" == *"post"* ]] && [[ $mageck
 	then
 		rm -r "$working_dir/mageck"
 		echo "No MAGeCK analysis performed: no non-library samples present"
-elif [[ ! -e mageck.config ]];
+elif [[ ! -e stats.config ]];
 	then
 		rm -r "$working_dir/mageck"
-		echo "ERROR: No MAGeCK analysis performed: mageck.config missing"
+		echo "ERROR: No MAGeCK analysis performed: stats.config missing"
 else
 	cd "$working_dir/mageck"
-	sed '1d' "$working_dir/mageck.config" > "$working_dir/mageck/mageck.config" #removes header from config file
+	sed '1d' "$working_dir/stats.config" | awk '{if(NF>0) {print $0}}' > "$working_dir/mageck/mageck.config" #removes header from config file and removes empty line, if any
+	
 	echo "Performing MAGeCK"
 	input="$working_dir/mageck/mageck.config"
 	while IFS= read -r line
@@ -37,4 +44,26 @@ else
 				mageck test -k "$working_dir/count/counts-aggregated.tsv" -t $test_sample -c $control_sample --sort-criteria neg -n $mageck_output/$mageck_output 2>> ../crispr.log
 		fi
 	done < "$input"
+fi
+
+#plot MAGeCK hits
+file_list=$(find . -name "*gene_summary.txt") #lists all paths to MAGeCK output files
+
+if [[ -n "$file_list" ]];
+	then
+		for file in $file_list
+		do
+			save_path=$(echo $file | sed 's|\(.*\)/.*|\1|') #removes file name from $file
+			save_path="${save_path}/"
+			Rscript "${SCRIPT_DIR}/plot-hits-mageck.R" $fdr $file $save_path
+			go_folder=$save_path"/GO-analysis-$go_test-$go_term"
+			if [[ $go_analysis == "True" ]];
+				then
+					if [[ ! -d $go_folder ]]
+						then
+							mkdir -p $go_folder
+							Rscript "${SCRIPT_DIR}/go.R" $email $file $fdr $species $go_folder $go_test $go_term
+					fi
+			fi
+		done
 fi
