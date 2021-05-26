acb0b1ae34852eb729cc18ddc8a1608528e1fd82
niekwit <41302859+niekwit@users.noreply.github.com>
2021-05-26 12:35:47 +0100

    installs required R packages

---
+++ R/install_r_packages.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+
+#check packages
+cran.packages <- c("ggplot2","yaml","ggrepel","viridis","dplyr")
+installed.packages <- installed.packages()[,1]
+
+for (i in cran.packages){
+  if(!i %in% installed.packages){
+    cat("R: ",i,"package missing, installing now\n")
+    install.packages(i,repos='http://cran.us.r-project.org')
+  }
+}
